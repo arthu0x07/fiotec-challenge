980d55a2567a04f2fd7134567bf5e553ebc7e576
Arthur Silva <arthur.santos.off@gmail.com>
2025-01-24 19:59:10 -0300

    feat: enhance ESLint and Prettier configurations; add TypeScript support and update rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,89 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import eslintPluginReact from 'eslint-plugin-react';
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
+
+import eslintPluginImport from 'eslint-plugin-import';
+import eslintPluginImportHelpers from 'eslint-plugin-import-helpers';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
 
 export default [
-  { ignores: ['dist'] },
-  {
-    files: ['**/*.{js,jsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    settings: { react: { version: '18.3' } },
-    plugins: {
-      react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-]
+	{
+		files: ['**/*.{js,jsx,ts,tsx}'],
+		languageOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'module',
+			parserOptions: {
+				ecmaFeatures: {
+					jsx: true,
+				},
+			},
+			globals: {
+				Atomics: 'readonly',
+				SharedArrayBuffer: 'readonly',
+			},
+		},
+		plugins: {
+			react: eslintPluginReact,
+			'react-hooks': eslintPluginReactHooks,
+			prettier: eslintPluginPrettier,
+			import: eslintPluginImport,
+			'import-helpers': eslintPluginImportHelpers,
+		},
+		rules: {
+			'prettier/prettier': 'error',
+			'react-hooks/rules-of-hooks': 'error',
+			'react-hooks/exhaustive-deps': 'error',
+			'import/extensions': [
+				'error',
+				'ignorePackages',
+				{
+					js: 'never',
+					jsx: 'never',
+					ts: 'never',
+					tsx: 'never',
+				},
+			],
+			'import-helpers/order-imports': [
+				'error',
+				{
+					newlinesBetween: 'always',
+					groups: [
+						'/react/',
+						'/^react-/',
+						'module',
+						'/^types/',
+						'/^components/',
+						'/^services/',
+						'/^layouts/',
+						'/^styled/',
+						'/^styles/',
+						'/^pages/',
+						'/^routes/',
+						'/^store/',
+						['parent', 'sibling', 'index'],
+					],
+					alphabetize: {
+						order: 'asc',
+						ignoreCase: true,
+					},
+				},
+			],
+			'@typescript-eslint/camelcase': 'off',
+			'@typescript-eslint/no-non-null-assertion': 'off',
+			'@typescript-eslint/no-explicit-any': 'off',
+			'react/prop-types': 'off',
+			'no-shadow': 'off',
+			'react/jsx-props-no-spreading': 'off',
+			'react/no-unescaped-entities': 'off',
+		},
+		settings: {
+			react: {
+				version: 'detect',
+			},
+			'import/resolver': {
+				node: {
+					extensions: ['.js', '.jsx', '.ts', '.tsx'],
+				},
+			},
+		},
+	},
+];
