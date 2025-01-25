a531a3b59b190dce8e7733db080f6e852535e73a
Arthur Silva <arthur.santos.off@gmail.com>
2025-01-25 16:42:43 -0300

    feat: add favorites management to ProjectsProvider with localStorage integration

--- src/context/ProjectsProvider.jsx
+++ src/context/ProjectsProvider.jsx
@@ -7,11 +7,12 @@ export const ProjectsContext = createContext();
 export function ProjectsProvider({ children }) {
 	const [projects, setProjects] = useState([]);
 	const [isLoading, setisLoading] = useState(true);
+	const [favoritesProjects, setFavoritesProjects] = useState([]);
 
 	useEffect(() => {
 		const fetchProjectsWithTimeout = async () => {
 			try {
-				await new Promise(resolve => setTimeout(resolve, 3000));
+				await new Promise(resolve => setTimeout(resolve, 1000));
 
 				const response = await axios.get('http://localhost:3000/projetos');
 				setProjects(response.data);
@@ -25,8 +26,37 @@ export function ProjectsProvider({ children }) {
 		fetchProjectsWithTimeout();
 	}, []);
 
+	useEffect(() => {
+		const favorites = JSON.parse(
+			window.localStorage.getItem('favorites') || '[]',
+		);
+		setFavoritesProjects(favorites);
+	}, [projects]);
+
+	const handleAddFavorite = projectId => {
+		const isAlreadyFavorite = favoritesProjects.includes(projectId);
+
+		if (isAlreadyFavorite) {
+			const updatedFavorites = favoritesProjects.filter(id => id !== projectId);
+			window.localStorage.setItem(
+				'favorites',
+				JSON.stringify(updatedFavorites),
+			);
+			setFavoritesProjects(updatedFavorites);
+		} else {
+			const updatedFavorites = [...favoritesProjects, projectId];
+			window.localStorage.setItem(
+				'favorites',
+				JSON.stringify(updatedFavorites),
+			);
+			setFavoritesProjects(updatedFavorites);
+		}
+	};
+
 	return (
-		<ProjectsContext.Provider value={{ projects, isLoading }}>
+		<ProjectsContext.Provider
+			value={{ projects, isLoading, handleAddFavorite, favoritesProjects }}
+		>
 			{children}
 		</ProjectsContext.Provider>
 	);
